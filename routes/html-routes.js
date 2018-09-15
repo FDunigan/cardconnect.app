417772ac4ccee52680172b969a9975e1e677dba9
Kevin Nathan <kevmaxnathan@gmail.com>
2018-09-15 00:32:55 -0400

    handlebars setup

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,29 +1,24 @@
 // *********************************************************************************
-// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// html-routes.js - this file offers a set of routes for sending users to the various handlebars templates
 // *********************************************************************************
 
 // Dependencies
-// =============================================================
 var path = require("path");
 
 // Routes
-// =============================================================
 module.exports = function(app) {
 
-  // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // index route loads view.html
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.render("index");
   });
 
-  app.get("/cms", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/views/cms.html"));
+  app.get("/config", function(req, res) {
+    res.render("config");
   });
 
-  // blog route loads blog.html
-  app.get("/blog", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
+  app.get("/pay", function(req, res) {
+    res.render("pay");
   });
 
 };
--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-// ==============================================================================
 // DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
-// ==============================================================================
-
+// require("dotenv").config();
 var express = require("express");
 var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
 
-// ==============================================================================
+
 // EXPRESS CONFIGURATION
-// ==============================================================================
-
 var app = express();
 
 // Sets an initial port
@@ -17,28 +13,25 @@ var PORT = process.env.PORT || 8080;
 
 var db = require("./models");
 
-
-// Sets up the Express app to handle data parsing
+// Middleware 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-// static directory
 app.use(express.static("public"));
 
-// ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
+// Handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }) );
+app.set("view engine", "handlebars");
 
+
+// ROUTER
 require("./routes/api-routes")(app);
 require("./routes/html-routes")(app);
 
-// =============================================================================
 // LISTENER with sequelize.sync
-// =============================================================================
 db.sequelize.sync({ force: true }).then(function(){
   app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
   });
 });
+
+module.exports = app; 
